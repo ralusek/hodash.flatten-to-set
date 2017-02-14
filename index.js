ef7d9734394de99e34b13b02da705e4d76a60f0f
ralusek <ralusek@gmail.com>
2017-02-14 02:17:39 -0800

    added deep flatten.

--- index.js
+++ index.js
@@ -4,14 +4,30 @@
 /**
  *
  */
-module.exports = function(iter) {
+module.exports = function flattenToSet(iter, deep) {
   const output = new Set();
+
+  if (!deep) {
+    iter.forEach(item => {
+      // Duck typing for flatten. Does not confirm if function.
+      if (item.forEach) {
+        item.forEach(item => output.add(item));
+      }
+      else output.add(item);
+    });
+  }
+  else deepFlattenToSet(iter, output);
+  
+  return output;
+}
+
+/**
+ *
+ */
+function deepFlattenToSet(iter, output) {
   iter.forEach(item => {
     // Duck typing for flatten. Does not confirm if function.
-    if (item.forEach) {
-      item.forEach(item => output.add(item));
-    }
+    if (item.forEach) deepFlattenToSet(item, output);
     else output.add(item);
   });
-  return output;
-};
+}
